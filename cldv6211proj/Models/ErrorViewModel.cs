0fe2ea3ba57b389c3a0390a74c26e36d42cc12ba
lab_services_student <lab_services_student@labVMH8OX>
2024-03-26 14:53:02 +0200

    Add project files.

---
+++ cldv6211proj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cldv6211proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
